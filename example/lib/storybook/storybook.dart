dcd062784363af0a27672e52b0cdf859dffefd62
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2024-05-07 12:22:05 +0530

    Updating example app

---
+++ example/lib/storybook/storybook.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:likeminds_chat_flutter_ui/likeminds_chat_flutter_ui.dart';
+
+class StoryBookApp extends StatefulWidget {
+  const StoryBookApp({super.key});
+
+  @override
+  State<StoryBookApp> createState() => _StoryBookAppState();
+}
+
+class _StoryBookAppState extends State<StoryBookApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: StoryBookPage(),
+    );
+  }
+}
+
+class StoryBookPage extends StatefulWidget {
+  const StoryBookPage({super.key});
+
+  @override
+  State<StoryBookPage> createState() => _StoryBookPageState();
+}
+
+class _StoryBookPageState extends State<StoryBookPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: iconStory(),
+      ),
+    );
+  }
+}
+
+LMChatText textStory() {
+  return const LMChatText(
+    'Hello, World!',
+    style: LMChatTextStyle(
+      selectable: true,
+      textStyle: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
+
+LMChatIcon iconStory() {
+  return const LMChatIcon(
+    type: LMChatIconType.icon,
+    icon: Icons.access_alarm,
+    style: LMChatIconStyle(
+      size: 48,
+      boxSize: 48,
+      color: Colors.red,
+      backgroundColor: Colors.green,
+    ),
+  );
+}
