3db5cc308f74481a359011f7625cc9ab2f8f0c49
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2024-08-07 02:19:34 +0530

    chore: add comments to media handler

--- lib/packages/core/lib/src/utils/media/media_handler.dart
+++ lib/packages/core/lib/src/utils/media/media_handler.dart
@@ -6,7 +6,11 @@ import 'package:likeminds_chat_fl/likeminds_chat_fl.dart';
 import 'package:likeminds_chat_flutter_core/src/utils/utils.dart';
 import 'package:likeminds_chat_flutter_ui/likeminds_chat_flutter_ui.dart';
 
+/// A class to manage all media picking, and accessing
+///
+/// Gives access to functions that handles media picking natively
 class LMChatMediaHandler {
+  /// A function that returns a List of videos from device storage
   static Future<LMResponse<List<LMChatMediaModel>>> pickVideos(
       int currentMediaLength) async {
     try {
@@ -66,50 +70,42 @@ class LMChatMediaHandler {
     }
   }
 
-  static Future<LMResponse<List<LMChatMediaModel>>> pickDocuments(
-      int currentMediaLength) async {
-    try {
-      final pickedFiles = await FilePicker.platform.pickFiles(
-        allowMultiple: true,
-        type: FileType.custom,
-        dialogTitle: 'Select files',
-        allowedExtensions: [
-          'pdf',
-        ],
-      );
-      if (pickedFiles != null) {
-        List<LMChatMediaModel> attachedFiles = [];
-        for (var pickedFile in pickedFiles.files) {
-          if (getFileSizeInDouble(pickedFile.size) > 100) {
-            return LMResponse(
-                success: false,
-                errorMessage: 'File size should be smaller than 100MB');
-          } else {
-            LMChatMediaModel documentFile;
-            documentFile = LMChatMediaModel(
-                mediaType: LMChatMediaType.document,
-                mediaFile: File(pickedFile.path!),
-                size: pickedFile.size,
-                meta: {
-                  'file_name': pickedFile.name,
-                });
+  /// A function that returns an image from device storage
+  static Future<LMResponse<LMChatMediaModel>> pickSingleImage() async {
+    final FilePickerResult? list = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+      type: FileType.image,
+    );
+    const double sizeLimit = 5;
 
-            attachedFiles.add(documentFile);
-          }
+    if (list != null && list.files.isNotEmpty) {
+      for (PlatformFile image in list.files) {
+        int fileBytes = image.size;
+        double fileSize = getFileSizeInDouble(fileBytes);
+        if (fileSize > sizeLimit) {
+          return LMResponse(
+            success: false,
+            errorMessage:
+                'Max file size allowed: ${sizeLimit.toStringAsFixed(2)}MB',
+          );
         }
-
-        return LMResponse(success: true, data: attachedFiles);
-      } else {
-        return LMResponse(success: true);
       }
-    } on Exception catch (err) {
-      return LMResponse(
-        success: false,
-        errorMessage: 'An error occurred\n${err.toString()}',
-      );
+      LMChatMediaModel mediaFile;
+
+      mediaFile = LMChatMediaModel(
+          mediaType: LMChatMediaType.image,
+          mediaFile: File(list.files.first.path!),
+          meta: {
+            'file_name': list.files.first.name,
+          });
+
+      return LMResponse(success: true, data: mediaFile);
+    } else {
+      return LMResponse(success: true);
     }
   }
 
+  /// A function that returns a List of images from device storage
   static Future<LMResponse<List<LMChatMediaModel>>> pickImages(
       int mediaCount) async {
     final FilePickerResult? list = await FilePicker.platform.pickFiles(
@@ -155,37 +151,48 @@ class LMChatMediaHandler {
     }
   }
 
-  static Future<LMResponse<LMChatMediaModel>> pickSingleImage() async {
-    final FilePickerResult? list = await FilePicker.platform.pickFiles(
-      allowMultiple: false,
-      type: FileType.image,
-    );
-    const double sizeLimit = 5;
+  /// A function that returns a List of documents from device storage
+  static Future<LMResponse<List<LMChatMediaModel>>> pickDocuments(
+      int currentMediaLength) async {
+    try {
+      final pickedFiles = await FilePicker.platform.pickFiles(
+        allowMultiple: true,
+        type: FileType.custom,
+        dialogTitle: 'Select files',
+        allowedExtensions: [
+          'pdf',
+        ],
+      );
+      if (pickedFiles != null) {
+        List<LMChatMediaModel> attachedFiles = [];
+        for (var pickedFile in pickedFiles.files) {
+          if (getFileSizeInDouble(pickedFile.size) > 100) {
+            return LMResponse(
+                success: false,
+                errorMessage: 'File size should be smaller than 100MB');
+          } else {
+            LMChatMediaModel documentFile;
+            documentFile = LMChatMediaModel(
+                mediaType: LMChatMediaType.document,
+                mediaFile: File(pickedFile.path!),
+                size: pickedFile.size,
+                meta: {
+                  'file_name': pickedFile.name,
+                });
 
-    if (list != null && list.files.isNotEmpty) {
-      for (PlatformFile image in list.files) {
-        int fileBytes = image.size;
-        double fileSize = getFileSizeInDouble(fileBytes);
-        if (fileSize > sizeLimit) {
-          return LMResponse(
-            success: false,
-            errorMessage:
-                'Max file size allowed: ${sizeLimit.toStringAsFixed(2)}MB',
-          );
+            attachedFiles.add(documentFile);
+          }
         }
+
+        return LMResponse(success: true, data: attachedFiles);
+      } else {
+        return LMResponse(success: true);
       }
-      LMChatMediaModel mediaFile;
-
-      mediaFile = LMChatMediaModel(
-          mediaType: LMChatMediaType.image,
-          mediaFile: File(list.files.first.path!),
-          meta: {
-            'file_name': list.files.first.name,
-          });
-
-      return LMResponse(success: true, data: mediaFile);
-    } else {
-      return LMResponse(success: true);
+    } on Exception catch (err) {
+      return LMResponse(
+        success: false,
+        errorMessage: 'An error occurred\n${err.toString()}',
+      );
     }
   }
 }
