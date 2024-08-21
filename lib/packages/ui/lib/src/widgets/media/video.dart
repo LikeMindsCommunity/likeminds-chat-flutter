ab8c88acc616b35642de3ab9555df4badbb5edff
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2024-08-21 21:46:37 +0530

    feat: add video boilerplate

---
+++ lib/packages/ui/lib/src/widgets/media/video.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LMChatVideo extends StatefulWidget {
+  const LMChatVideo({super.key});
+
+  @override
+  State<LMChatVideo> createState() => _LMChatVideoState();
+}
+
+class _LMChatVideoState extends State<LMChatVideo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
