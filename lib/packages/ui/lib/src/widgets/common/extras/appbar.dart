a8e10bec4d9a98f053149217935f951b5336d6d2
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2024-09-16 16:47:22 +0530

    chore: refactor appbar

--- lib/packages/ui/lib/src/widgets/common/extras/appbar.dart
+++ lib/packages/ui/lib/src/widgets/common/extras/appbar.dart
@@ -3,7 +3,9 @@ import 'package:likeminds_chat_flutter_ui/src/theme/theme.dart';
 import 'package:likeminds_chat_flutter_ui/src/utils/utils.dart';
 import 'package:likeminds_chat_flutter_ui/src/widgets/widgets.dart';
 
+/// A custom AppBar for chat interfaces.
 class LMChatAppBar extends StatelessWidget implements PreferredSizeWidget {
+  /// Creates an instance of [LMChatAppBar].
   const LMChatAppBar({
     super.key,
     this.leading,
@@ -16,15 +18,28 @@ class LMChatAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.bottom,
   });
 
+  /// The widget to display as the leading element.
   final Widget? leading;
+
+  /// The list of widgets to display at the end of the AppBar.
   final List<Widget>? trailing;
+
+  /// The main title of the AppBar.
   final Widget? title;
+
+  /// The subtitle displayed below the title.
   final Widget? subtitle;
+
+  /// An optional banner widget.
   final Widget? banner;
+
+  /// An optional bottom widget.
   final PreferredSizeWidget? bottom;
 
+  /// Callback function for the back button.
   final Function? backButtonCallback;
 
+  /// The style configuration for the AppBar.
   final LMChatAppBarStyle? style;
 
   @override
@@ -126,6 +141,7 @@ class LMChatAppBar extends StatelessWidget implements PreferredSizeWidget {
     );
   }
 
+  /// Creates a copy of the current [LMChatAppBar] with the given parameters.
   LMChatAppBar copyWith({
     Widget? leading,
     List<Widget>? trailing,
@@ -152,18 +168,9 @@ class LMChatAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => Size.fromHeight(style?.height ?? 72);
 }
 
+/// Style configuration for [LMChatAppBar].
 class LMChatAppBarStyle {
-  final Color? backgroundColor;
-  final double? height;
-  final double? width;
-  final double? gap;
-  final Border? border;
-  final EdgeInsets? padding;
-  final EdgeInsets? margin;
-  final List<BoxShadow>? shadow;
-  final bool centerTitle;
-  final bool showBackButton;
-
+  /// Creates an instance of [LMChatAppBarStyle].
   const LMChatAppBarStyle({
     this.backgroundColor,
     this.border,
@@ -177,6 +184,37 @@ class LMChatAppBarStyle {
     this.showBackButton = true,
   });
 
+  /// Background color of the AppBar.
+  final Color? backgroundColor;
+
+  /// Height of the AppBar.
+  final double? height;
+
+  /// Width of the AppBar.
+  final double? width;
+
+  /// Gap between elements in the AppBar.
+  final double? gap;
+
+  /// Border of the AppBar.
+  final Border? border;
+
+  /// Padding inside the AppBar.
+  final EdgeInsets? padding;
+
+  /// Margin outside the AppBar.
+  final EdgeInsets? margin;
+
+  /// Shadow effects for the AppBar.
+  final List<BoxShadow>? shadow;
+
+  /// Whether to center the title.
+  final bool centerTitle;
+
+  /// Whether to show the back button.
+  final bool showBackButton;
+
+  /// Creates a copy of the current [LMChatAppBarStyle] with the given parameters.
   LMChatAppBarStyle copyWith({
     Color? backgroundColor,
     double? height,
@@ -203,6 +241,7 @@ class LMChatAppBarStyle {
     );
   }
 
+  /// A basic style for the AppBar.
   factory LMChatAppBarStyle.basic() {
     return const LMChatAppBarStyle(
       backgroundColor: LMChatDefaultTheme.container,
