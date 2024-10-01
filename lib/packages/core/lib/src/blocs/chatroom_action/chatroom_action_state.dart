6dd3ef009d5fa736dd4aaacf3fee72e432dc2940
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2024-10-01 15:34:22 +0530

    feat: add business logic to make reactions work

--- lib/packages/core/lib/src/blocs/chatroom_action/chatroom_action_state.dart
+++ lib/packages/core/lib/src/blocs/chatroom_action/chatroom_action_state.dart
@@ -1,30 +1,44 @@
 part of 'chatroom_action_bloc.dart';
 
 @immutable
-abstract class ChatroomActionState extends Equatable {}
+abstract class LMChatroomActionState extends Equatable {}
 
-class ChatroomActionInitial extends ChatroomActionState {
+class ChatroomActionInitial extends LMChatroomActionState {
   @override
   List<Object?> get props => [];
 }
 
-class ChatroomActionLoading extends ChatroomActionState {
+class ChatroomActionLoading extends LMChatroomActionState {
   @override
   List<Object?> get props => [];
 }
 
-class ChatroomTopicSet extends ChatroomActionState {
+class ChatroomTopicSet extends LMChatroomActionState {
   final Conversation topic;
   ChatroomTopicSet(this.topic);
   @override
   List<Object?> get props => [topic];
 }
 
-class ChatroomTopicError extends ChatroomActionState {
+class ChatroomTopicError extends LMChatroomActionState {
   final String errorMessage;
   ChatroomTopicError({
     required this.errorMessage,
   });
   @override
   List<Object?> get props => [errorMessage];
+}
+
+class LMChatShowEmojiKeyboardState extends LMChatroomActionState {
+  final int conversationId;
+
+  LMChatShowEmojiKeyboardState({required this.conversationId});
+  @override
+  List<Object?> get props => [conversationId];
+}
+
+class LMChatHideEmojiKeyboardState extends LMChatroomActionState {
+  LMChatHideEmojiKeyboardState();
+  @override
+  List<Object?> get props => [];
 }
